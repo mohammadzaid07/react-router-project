7b5ef5cf68a8879c40d069782c63785ef806678f
Mohammad Zaid <mohdzaid07091997@gmail.com>
2025-05-16 22:40:55 +0530

    Home Page Route Changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" className="flex items-center">
+          <Link to="react-router-project/" className="flex items-center">
             <img
               src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
               className="mr-3 h-12"
@@ -34,7 +34,7 @@ function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <NavLink
-                  to="/"
+                  to="react-router-project/"
                   className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}  lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
